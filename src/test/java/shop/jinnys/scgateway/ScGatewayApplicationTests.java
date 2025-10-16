635195e6659983376c55e0526443e34e5794e1eb
hyunsoojin <jhs7251@naver.com>
2025-10-16 09:40:56 +0900

    generate project

---
+++ src/test/java/shop/jinnys/scgateway/ScGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.jinnys.scgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
